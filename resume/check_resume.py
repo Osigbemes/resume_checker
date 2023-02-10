73f7e7b424fa79806dff424d361eaa6b8ffe59c6
Osinaike Gbemileke <osinaikegbemileke@gmail.com>
2023-02-10 10:35:01 +0100

    schedule appointment

--- resume/check_resume.py
+++ resume/check_resume.py
@@ -16,17 +16,9 @@ nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
  
-# file_directory = f'/media/{doc_name}'
 
 def extract_text_from_pdf(pdf_path):
     return extract_text(pdf_path)
- 
- 
-# if __name__ == '__main__':
-#     print(extract_text_from_pdf('./resume.pdf'))  # noqa: T001
-    
-
-#############################################################
  
  
 def extract_text_from_docx(docx_path):
@@ -59,41 +51,11 @@ def get_names(file_path):
     
     return names[0]
 
- 
- 
-# if __name__ == '__main__':
-#     text = extract_text_from_pdf('resume.pdf')
-#     names = extract_names(text)
- 
-#     if names:
-#         print(names[0])  # noqa: T001
- 
-
 
  
 PHONE_REG = re.compile(r'[\+\(]?[1-9][0-9 .\-\(\)]{8,}[0-9]')
  
  
-# def doc_to_text_catdoc(file_path):
-#     try:
-#         process = subprocess.Popen(  # noqa: S607,S603
-#             ['catdoc', '-w', file_path],
-#             stdout=subprocess.PIPE,
-#             stderr=subprocess.PIPE,
-#             universal_newlines=True,
-#         )
-#     except (
-#         FileNotFoundError,
-#         ValueError,
-#         subprocess.TimeoutExpired,
-#         subprocess.SubprocessError,
-#     ) as err:
-#         return (None, str(err))
-#     else:
-#         stdout, stderr = process.communicate()
- 
-#     return (stdout.strip(), stderr.strip())
-
 def extract_text_from_pdf(pdf_path):
     return extract_text(pdf_path)
  
@@ -115,19 +77,6 @@ def get_phonenumber(file_name):
     
     return phone_number
 
-
-# if __name__ == '__main__':
-#     text = extract_text_from_pdf('resume.pdf')
-#     phone_number = extract_phone_number(text)
- 
-#     print(phone_number)  # noqa: T001
-    
-    
-    
-    
-    
-    
-# example_06.py
  
 
  
@@ -148,23 +97,8 @@ def get_emails(file_path):
     
     return emails[0]
  
-# if __name__ == '__main__':
-#     text = extract_text_from_pdf('resume.pdf')
-#     emails = extract_emails(text)
- 
-#     if emails:
-#         print(emails[0])  # noqa: T001
-        
-        
-        
-        
-        
-# example_07.py
- 
 
  
-# you may read the database from a csv file or some other database
-
  
  
 def extract_text_from_docx(docx_path):
@@ -212,84 +146,6 @@ def get_result(file_path, role):
     text = extract_text_from_pdf(file_path)
     skills = extract_skills(text, role)
     return skills
-
-# if __name__ == '__main__':
-#     text = extract_text_from_pdf('resume.pdf')
-#     skills = extract_skills(text)
- 
-#     print("these are the skils", skills)  # noqa: T001
-    
-    
-
-# example_08.py
- 
- #######################3 commented out because of api call failing
- 
-# def extract_text_from_docx(docx_path):
-#     txt = docx2txt.process(docx_path)
-#     if txt:
-#         return txt.replace('\t', ' ')
-#     return None
- 
-# def extract_text_from_pdf(pdf_path):
-#     return extract_text(pdf_path)
-
-
-# def skill_exists(skill):
-#     url = f'https://api.apilayer.com/skills?q={skill}&amp;count=1'
-#     headers = {'apikey': 'YOUR API KEY'}
-#     response = requests.request('GET', url, headers=headers)
-#     result = response.json()
- 
-#     if response.status_code == 200:
-#         return len(result) &gt; 0 and result[0].lower() == skill.lower()
-#     raise Exception(result.get('message'))
- 
- 
-# def extract_skills(input_text):
-#     stop_words = set(nltk.corpus.stopwords.words('english'))
-#     word_tokens = nltk.tokenize.word_tokenize(input_text)
- 
-#     # remove the stop words
-#     filtered_tokens = [w for w in word_tokens if w not in stop_words]
- 
-#     # remove the punctuation
-#     filtered_tokens = [w for w in word_tokens if w.isalpha()]
- 
-#     # generate bigrams and trigrams (such as artificial intelligence)
-#     bigrams_trigrams = list(map(' '.join, nltk.everygrams(filtered_tokens, 2, 3)))
- 
-#     # we create a set to keep the results in.
-#     found_skills = set()
- 
-#     # we search for each token in our skills database
-#     for token in filtered_tokens:
-#         if skill_exists(token.lower()):
-#             found_skills.add(token)
- 
-#     # we search for each bigram and trigram in our skills database
-#     for ngram in bigrams_trigrams:
-#         if skill_exists(ngram.lower()):
-#             found_skills.add(ngram)
- 
-#     return found_skills
- 
- 
-# if __name__ == '__main__':
-#     text = extract_text_from_pdf('resume.pdf')
-#     skills = extract_skills(text)
- 
-#     print(skills)  # noqa: T001
-    
-    
-    
-    
-    
-    
-    
-# example_09.py
- 
-
  
 RESERVED_WORDS = [
     'school',
@@ -340,11 +196,11 @@ def extract_education(input_text):
     return education
  
  
-if __name__ == '__main__':
-    text = extract_text_from_pdf('resume.pdf')
-    education_information = extract_education(text)
+# if __name__ == '__main__':
+#     text = extract_text_from_pdf('resume.pdf')
+#     education_information = extract_education(text)
  
-    print(education_information)  # noqa: T001
+#     print(education_information)  # noqa: T001
     
     
     
@@ -403,10 +259,10 @@ def extract_education(input_text):
     return education
  
  
-if __name__ == '__main__':
-    text = extract_text_from_pdf('resume.pdf')
-    education_information = extract_education(text)
+# if __name__ == '__main__':
+#     text = extract_text_from_pdf('resume.pdf')
+#     education_information = extract_education(text)
  
-    print(education_information)  # noqa: T001
+#     print(education_information)  # noqa: T001
  
  
